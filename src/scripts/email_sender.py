875d198444cc3c7c1fd0f4378554669d7af834c4
renan <renan.rgsilva@gmail.com>
2022-05-17 03:17:59 +0200

    Adding packaging config

--- src/scripts/email_sender.py
+++ src/scripts/email_sender.py
@@ -5,13 +5,13 @@ from datetime import datetime
 
 
 def send_bill_notification():
-    while True:
-        current_time = datetime.now()
-        if current_time.hour == 8 and current_time.minute == 30:
-            bill_service = TauronService()
-            email_service = EmailService()
-            bill = bill_service.get_next_bill()
-            email_service.send_bill_notification(bill)
+    # while True:
+        # current_time = datetime.now()
+    # if current_time.hour == 8 and current_time.minute == 30:
+    bill_service = TauronService()
+    email_service = EmailService()
+    bill = bill_service.get_next_bill()
+    email_service.send_bill_notification(bill)
 
 
 if __name__ == "__main__":
