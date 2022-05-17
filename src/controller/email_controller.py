b53841ce6cb883bf66e5fcc498c95ae709b10811
renan <renan.rgsilva@gmail.com>
2022-05-17 22:44:44 +0200

    Adding docstring for email_controller module

--- src/controller/email_controller.py
+++ src/controller/email_controller.py
@@ -1,14 +1,20 @@
+"""email controller
+
+"""
 from flask import Flask
 from service.email_service import EmailService
-
 from service.tauron_scrape_service import TauronService
-from example_package import example
 
 app = Flask(__name__)
 
 
 @app.route('/api/v1/send-bill-notification')
 def send_bill_notification():
+    """Send email tiwh notification of urgent bill
+
+    Returns:
+        _type_: View content
+    """
     bill_service = TauronService()
     email_service = EmailService()
     bill = bill_service.get_next_bill()
